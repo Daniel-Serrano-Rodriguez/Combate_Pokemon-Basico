bb78bcdaf5ea9f0e8fd9584fe751def0cb5f154b
Daniel Serrano Rodriguez <daniel.serranorodriguez00@gmail.com>
2021-12-27 19:01:43 +0100

    Creada la clase 'Almacen', que guarda los Pokemons y los Movimientos

---
+++ src/utils/Almacen.java
@@ -0,0 +1,11 @@
+package utils;
+
+import java.util.ArrayList;
+
+import models.AbstractMove;
+import models.Pokemon;
+
+public class Almacen {
+	public static ArrayList<Pokemon> almacenPokemon = new ArrayList<Pokemon>();
+	public static ArrayList<AbstractMove> almacenMovimientos = new ArrayList<AbstractMove>();
+}
