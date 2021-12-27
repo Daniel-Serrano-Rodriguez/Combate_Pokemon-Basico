f62bc9b4c472ede6f56d4ea5c24d45e5e5b950cb
Daniel Serrano Rodriguez <daniel.serranorodriguez00@gmail.com>
2021-12-27 13:23:26 +0100

    Introducidos todos los estados que se pueden aplicar a los pokemons.

---
+++ src/utils/Estado.java
@@ -0,0 +1,9 @@
+package utils;
+
+public enum Estado {
+	Ninguno, Quemado, Congelado, Paralisis, Envenenado, Env_Grave, Atado, Sin_Huida, Confusion, Maldicion, Somnoliento,
+	Embargo, Otra_Vez, Retroceder, Anti_cura, Identificar, Enamorado, Drenadoras, Pesadilla, Canto_Mortal, Mofa,
+	Telequinesis, Tormento, Cambia_Tipo, Anillo_Aquatico, Prepararse, Cargando, Centro_Atencion, Enrolle_Defensivo,
+	Enraizado, Capa_Magica, Levitacion_Magnetica, Mimica, Minimizar, Proteccion, Recarga, Sustituto, Apuntando, Terco,
+	Transformado
+}
