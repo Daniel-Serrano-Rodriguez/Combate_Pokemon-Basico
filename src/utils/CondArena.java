3098af99332e91dcce771dcb486c28b20b72e0e9
Daniel Serrano Rodriguez <daniel.serranorodriguez00@gmail.com>
2021-12-27 13:59:26 +0100

    Creadas las condiciones de la arena y la condicion de entrada al combate

---
+++ src/utils/CondArena.java
@@ -0,0 +1,5 @@
+package utils;
+
+public enum CondArena {
+	TormentaArena, Granizar, Soleado, Aura_Oscura, Ventolera, Neblina, Lluvia
+}
---
+++ src/utils/CondPosiPkmn.java
@@ -0,0 +1,5 @@
+package utils;
+
+public enum CondPosiPkmn {
+	Pinchos, Piedras_Afiladas, Pinchos_Venenosos, Tela_Arana, Acero_Afilado
+}
